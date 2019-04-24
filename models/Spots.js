1bea753ecff38de3acebc9b8e5c1ab2443a656d6
ernestogl88 <ernestogomezlopez@gmail.com>
2019-04-24 17:50:13 +0200

    Insertion finalized

--- models/Spots.js
+++ models/Spots.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const spotSchema = new Schema({
-  coords: {
-    lng: Number,
-    lat: Number
-  },
-  picture: String,
+  lng: {type: String, required:true},
+  lat: {type: String, required:true},
+  picture: {type: String, required:true},
   spotDescription: String
 });
 
-const Spots = mongoose.model('Spots', spotSchema);
+const Spots = mongoose.model("Spots", spotSchema);
 module.exports = Spots;
