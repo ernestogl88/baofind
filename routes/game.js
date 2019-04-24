e97ad18ae3cb83e2b1b2205353a063f08003c2d7
ernestogl88 <ernestogomezlopez@gmail.com>
2019-04-24 15:45:24 +0200

    Hecha la inserción teóricamente

--- routes/game.js
+++ routes/game.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const router  = express.Router();
-const Game = require('../models/Game')
-const request = require('request-promise');
-const uploadCloud = require('../config/cloudinary.js');
-const Spot = require('../models/Spots')
-const multer  = require('multer');
-const Map = require('../models/Map');
-const Spot  = require('../models/Spots');
-const User = request('../models/User.js');
+const express = require("express");
+const router = express.Router();
+const Game = require("../models/Game");
+const request = require("request-promise");
+const uploadCloud = require("../config/cloudinary.js");
+const Spot = require("../models/Spots");
+const multer = require("multer");
+const Map = require("../models/Map");
+const User = require("../models/User.js");
 
-router.get('/newGame', (req,res)=>{
-  res.render('game/newGame')
-})
+router.get("/newGame", (req, res) => {
+  res.render("game/newGame");
+});
 
-router.post('/newGame', (req,res)=>{
-  let spots = []
-  for (var i=0; i<req.body.totalSpots;i++){
+router.post("/newGame", (req, res) => {
+  let spotsId = [];
+  let spots = [];
+  for (var i = 0; i < req.body.totalSpots; i++) {
     let lng = `lng${i}`;
     let lat = `lat${i}`;
     let picture = `image${i}`;
@@ -32,41 +32,64 @@ router.post('/newGame', (req,res)=>{
     });
     spots.push(spot);
   }
-  console.log(spots);
-})
-
-router.get('/joinGame', (req,res)=>{
-  res.render('game/joinGame')
-})
-
-router.get('/nearPlaces/:lat/:long', (req,res)=>{
-  let lat = req.params.lat;
-  let long = req.params.long;
-  request(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=100&key=AIzaSyCekv9TIkClXh5EfD8V7pObO2gTrs_g__A`)
-  .then(nearestPoint => res.json(JSON.parse(nearestPoint)))
-  .catch(err => {
-      console.log(err);
+  let game = new Game({
+    name: req.body.gameTitle,
+    users: [req.user._id],
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    reward: req.body.gameReward
   });
-})
-
-
-router.get('/clue', (req,res) => {
-  res.render('game/clue')
-})
-
-router.post('/clue', uploadCloud.single('photo'), (req, res) => {
-  const { picture } = req.body;
-  const imgPath = req.file.url;
-  const imgName = req.file.originalname;
-  const photo = new Spot({picture, description, imgPath, imgName})
-  photo.save()
-  .then(spot => {
-    res.render('game/clue');
-  })
-  .catch(error => {
-    console.log(error);
-  })
+  spots.forEach(spot => {
+    spot
+      .save()
+      .then(spot => {
+        spotsId.push(spot._id);
+      })
+      .catch(err => console.log(err));
+  });
+  let map = new Map({
+    spots: spotsId,
+    mapDescription: req.body.gameDescription
+  });
+  map.save().then(map => {
+    game.map = map._id;
+    return game.save()
+    .then(res.render('/'))
+    .catch(err=>console.log(err))
+  });
 });
 
+router.get("/joinGame", (req, res) => {
+  res.render("game/joinGame");
+});
+
+router.get("/nearPlaces/:lat/:long", (req, res) => {
+  let lat = req.params.lat;
+  let long = req.params.long;
+  request(
+    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=100&key=AIzaSyCekv9TIkClXh5EfD8V7pObO2gTrs_g__A`
+  )
+    .then(nearestPoint => res.json(JSON.parse(nearestPoint)))
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/clue", (req, res) => {
+  res.render("game/clue");
+});
+
+router.post("/clue", uploadCloud.single("photo"), (req, res) => {
+  const { picture } = req.body;
+  const imgPath = req.file.url;
+  User.findByIdandUpdate(req.user.id, {
+    $push: { pictures: imgPath }})
+    .then(spot => {
+      res.render("game/clue");
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
 module.exports = router;
