a10e298e84f3e6736fe7a20752f7020e1f8e16c7
Alberto González <algorinetportfolio@gmail.com>
2019-04-24 15:13:42 +0200

    photo coords added

--- public/javascripts/metadata.js
+++ public/javascripts/metadata.js
@@ -3,12 +3,22 @@ window.onload = getExif;
 function getExif() {
   document.getElementById("photoUpload").onchange = function(e) {
     var file = e.target.files[0];
+    var photoPosition = []
     if (file && file.name) {
       EXIF.getData(file, function() {
-        var exifData = EXIF.pretty(this);
-        console.log(exifData);
+        var exifData = EXIF.getAllTags(this);
         if (exifData) {
-          alert(exifData);
+          var latdegrees = exifData.GPSLatitude[0]
+          var latminutes = exifData.GPSLatitude[1]
+          var latseconds = exifData.GPSLatitude[2]
+          var photoLat = (latseconds/3600) + (latminutes/60) + latdegrees
+          photoPosition.push(photoLat)
+          var lngdegrees = exifData.GPSLongitude[0]
+          var lngminutes = exifData.GPSLongitude[1]
+          var lngseconds = exifData.GPSLongitude[2]
+          var photoLng = (lngseconds/3600) + (lngminutes/60) + lngdegrees
+          photoPosition.push(photoLng);
+          return photoPosition;
         } else {
           alert("No EXIF data found in image '" + file.name + "'.");
         }
